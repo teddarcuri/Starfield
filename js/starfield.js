368860ae0f8ee004d4ba6de3dbe14adb480ca246
Tedd Arcuri <tarcuri88@gmail.com>
2014-03-09 20:11:04 -0600

    Reverted to measuring canvas size instead of viewport dimensions.

--- js/starfield.js
+++ js/starfield.js
@@ -7,6 +7,8 @@ function Starfield(min, max) {
 
 	// Get starfield dimensions
 	this.canvas = document.getElementById("starfield");
+	this.canvasHeight = this.canvas.offsetHeight;
+	this.canvasWidth = this.canvas.offsetWidth;
 
 	// Create Stars
 	for ( var i = 0; i < this.stars ; i++ ) {
@@ -20,20 +22,20 @@ function Starfield(min, max) {
 		var randStarType = Math.floor(Math.random() * 4); // Generate random key 0-4
 
 		// Generate random placement
-		var coordinateX = Math.floor(Math.random() * 100);
-		var coordinateY = Math.floor(Math.random() * 100);
+		var coordinateX = Math.floor(Math.random() * this.canvasWidth);
+		var coordinateY = Math.floor(Math.random() * this.canvasHeight);
 
 		// Generate random twinkle
 		var animationFrequency = Math.random() * (10 - 2) + 2;
 		var animationDelay = Math.floor(Math.random() * 2);
 		
 		// Populate Starfield
-		this.canvas.innerHTML += '<div style="margin-left:' + coordinateX + '%; margin-top:' + coordinateY + '%; -webkit-animation: twinkle ' + animationFrequency  + "s " + animationDelay  + 's infinite; -moz-animation: twinkle ' + animationFrequency  + "s " + animationDelay  + 's infinite; animation: twinkle ' + animationFrequency  + "s " + animationDelay  + 's infinite; " class="' + starTypes[randStarType] + '"></div>' ;	
+		this.canvas.innerHTML += '<div style="margin-left:' + coordinateX + 'px; margin-top:' + coordinateY + 'px; -webkit-animation: twinkle ' + animationFrequency  + "s " + animationDelay  + 's infinite; -moz-animation: twinkle ' + animationFrequency  + "s " + animationDelay  + 's infinite; animation: twinkle ' + animationFrequency  + "s " + animationDelay  + 's infinite; " class="' + starTypes[randStarType] + '"></div>' ;	
 	} 
 }
 
 // Create the starfield
-var starfield = new Starfield(200, 300);
+var starfield = new Starfield(300, 600);
 
 
 
